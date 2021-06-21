522161a0eedfd70d68fe174c8708ac317736b0e8
root <you@example.com>
2021-06-21 03:17:11 -0700

    copypaaa

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,15 +2,14 @@
 /*description of main function*/
 int main(void)
 {
-  int ch;
-  for(ch = 'a'; ch <= 'z'; ch++)
+int ch;
+for(ch = 'a'; ch <= 'z'; ch++)
     {
       if(ch != 'q' && ch != 'e')
 	{
 	  putchar(ch);
 	}
-      
-    }
-  putchar('\n');
+     }
+putchar('\n');
   return(0);
 }
